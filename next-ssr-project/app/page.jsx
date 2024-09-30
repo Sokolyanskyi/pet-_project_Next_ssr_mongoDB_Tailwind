e9ba58f44997e42d627c577c62bf4259a800c0bc
Yaroslav <9riksok@gmail.com>
2024-10-01 02:25:20 +0300

    changes

--- next-ssr-project/app/page.jsx
+++ next-ssr-project/app/page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
+import { useAmp } from "next/amp";
 
 export const metadata = {
   title: "HomePage",
 };
 const HomePage = () => {
+  useAmp;
   return (
     <>
       <Hero />
